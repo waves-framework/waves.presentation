1ce9587db5d8fc8ab496a0131192c27468cdabb1
Egor Khindikaynen <khek@ambertape.ru>
2020-04-16 13:25:51 +0300

    FLUID-79 Presentation: Cleaned up code.

--- sources/Fluid.Presentation/Interfaces/IPresentation.cs
+++ sources/Fluid.Presentation/Interfaces/IPresentation.cs
@@ -3,22 +3,22 @@
 namespace Fluid.Presentation.Interfaces
 {
     /// <summary>
-    /// Interface for presentations.
+    ///     Interface for presentations.
     /// </summary>
     public interface IPresentation : INotifyPropertyChanged
     {
         /// <summary>
-        /// Gets view model context.
+        ///     Gets view model context.
         /// </summary>
         IPresentationViewModel DataContext { get; }
 
         /// <summary>
-        /// Gets view.
+        ///     Gets view.
         /// </summary>
         IPresentationView View { get; }
 
         /// <summary>
-        /// Initializes presentation.
+        ///     Initializes presentation.
         /// </summary>
         void Initialize();
     }
--- sources/Fluid.Presentation/Interfaces/IPresentationViewModel.cs
+++ sources/Fluid.Presentation/Interfaces/IPresentationViewModel.cs
@@ -3,12 +3,12 @@
 namespace Fluid.Presentation.Interfaces
 {
     /// <summary>
-    /// Interface for presentation view models.
+    ///     Interface for presentation view models.
     /// </summary>
     public interface IPresentationViewModel : IObservableObject
     {
         /// <summary>
-        /// Initializes presentation view model.
+        ///     Initializes presentation view model.
         /// </summary>
         void Initialize();
     }
